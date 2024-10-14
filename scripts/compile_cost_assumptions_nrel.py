550e4a82d09f833cfd95b42d9289a574537a62e4
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-10-14 18:10:59 +0200

    code: re-factor

--- scripts/compile_cost_assumptions_nrel.py
+++ scripts/compile_cost_assumptions_nrel.py
@@ -2,47 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import pathlib
 from _helpers import mock_snakemake
 
 
-def read_atb_input_file(input_file_list):
-    dataframe_list = []
-    for file in input_file_list:
-        dataframe_list.append(pd.read_parquet(file))
-    return dataframe_list
-
-
-def filter_input_file(input_file_list, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep):
-    atb_input_df_list = read_atb_input_file(input_file_list)
+def filter_input_file(input_file_path, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep):
+    atb_input_df = pd.read_parquet(input_file_path)
     list_core_metric_parameter_to_keep = [str(x).casefold() for x in list_core_metric_parameter_to_keep]
     list_years = [str(x).casefold() for x in list_years]
 
     # --> select columns
-    atb_input_df_2022 = atb_input_df_list[0].loc[:, list_columns_to_keep]
-    atb_input_df_2024 = atb_input_df_list[1].loc[:, list_columns_to_keep]
+    for column_name in list_columns_to_keep:
+        if column_name not in atb_input_df:
+            print("missing column", column_name)
+            atb_input_df[column_name] = "not_available"
+        else:
+            pass
+    atb_input_df = atb_input_df.loc[:, list_columns_to_keep]
 
     # --> select rows based on core_metric_parameter
     # Note: we do not apply any selection on core_metric_case and scenario.
     # Such selection can be done in the model config (e.g. PyPSA-Earth)
-    atb_input_df_2022 = atb_input_df_2022.loc[
-        atb_input_df_2022["core_metric_parameter"].str.casefold().isin(list_core_metric_parameter_to_keep)]
-    atb_input_df_2024 = atb_input_df_2024.loc[
-        atb_input_df_2024["core_metric_parameter"].str.casefold().isin(list_core_metric_parameter_to_keep)]
+    atb_input_df = atb_input_df.loc[atb_input_df["core_metric_parameter"].str.casefold().isin(list_core_metric_parameter_to_keep)]
 
     # --> select rows based on core_metric_variable
-    # Note: 2020 data are fetched from the input file atb_e_2022.*
-    atb_input_df_2022 = atb_input_df_2022.loc[
-        atb_input_df_2022["core_metric_variable"].astype(str).str.casefold() == list_years[0]]
-
-    # Note: 2025, 2030, 2035, 2040, 2045, 2050 data are fetched from the input file atb_e_2024.*
-    atb_input_df_2024 = atb_input_df_2024.loc[
-        atb_input_df_2024["core_metric_variable"].astype(str).str.casefold().isin(list_years[1:])]
+    if input_file_path.stem == "atb_e_2022":
+        # Note: 2020 data are fetched from the input file atb_e_2022.
+        atb_input_df = atb_input_df.loc[atb_input_df["core_metric_variable"].astype(str).str.casefold() == list_years[0]]
+    elif input_file_path.stem == "atb_e_2024":
+        # Note: 2025, 2030, 2035, 2040, 2045, 2050 data are fetched from the input file atb_e_2024.*
+        atb_input_df = atb_input_df.loc[atb_input_df["core_metric_variable"].astype(str).str.casefold().isin(list_years[1:])]
+    else:
+        raise Exception("{} - the input file considered is not among the needed ones: atb_e_2022.parquet, atb_e_2024.parquet".format(input_file_path.stem))
 
     # --> drop duplicated rows
-    atb_input_df_2022 = atb_input_df_2022.drop_duplicates(keep="first")
-    atb_input_df_2024 = atb_input_df_2024.drop_duplicates(keep="first")
+    atb_input_df = atb_input_df.drop_duplicates(keep="first")
 
-    return atb_input_df_2022, atb_input_df_2024
+    return atb_input_df
 
 
 def calculate_fom_percentage(x, dataframe):
@@ -53,25 +49,9 @@ def calculate_fom_percentage(x, dataframe):
 
     if x["core_metric_parameter"].casefold() == "fixed o&m":
         if "retrofit" in x["technology"].casefold():
-            fom_perc_value = x.value / dataframe.query(
-                "core_metric_parameter.str.casefold() == 'additional occ' & "
-                "core_metric_case == @x.core_metric_case & "
-                "core_metric_variable == @x.core_metric_variable & "
-                "technology == @x.technology & "
-                "technology_alias == @x.technology_alias & "
-                "techdetail == @x.techdetail & "
-                "display_name == @x.display_name & "
-                "scenario == @x.scenario")["value"]*100.0
+            fom_perc_value = x.value / dataframe.query("atb_year == @x.atb_year & core_metric_parameter.str.casefold() == 'additional occ' & core_metric_case == @x.core_metric_case & core_metric_variable == @x.core_metric_variable & technology == @x.technology & technology_alias == @x.technology_alias & techdetail == @x.techdetail & display_name == @x.display_name & scenario == @x.scenario")["value"]*100.0
         else:
-            fom_perc_value = x.value / dataframe.query(
-                "core_metric_parameter.str.casefold() == 'capex' & "
-                "core_metric_case == @x.core_metric_case & "
-                "core_metric_variable == @x.core_metric_variable & "
-                "technology == @x.technology & "
-                "technology_alias == @x.technology_alias & "
-                "techdetail == @x.techdetail & "
-                "display_name == @x.display_name & "
-                "scenario == @x.scenario")["value"]*100.0
+            fom_perc_value = x.value / dataframe.query("atb_year == @x.atb_year & core_metric_parameter.str.casefold() == 'capex' & core_metric_case == @x.core_metric_case & core_metric_variable == @x.core_metric_variable & technology == @x.technology & technology_alias == @x.technology_alias & techdetail == @x.techdetail & display_name == @x.display_name & scenario == @x.scenario")["value"]*100.0
         return round(fom_perc_value.values[0], 2)
     else:
         return x.value
@@ -90,7 +70,13 @@ def concatenate_columns(dataframe, column_name, column_name_list):
 
 def pre_process_input_file(input_file_list, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep, nrel_source):
 
-    atb_input_df_2022, atb_input_df_2024 = filter_input_file(input_file_list, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep)
+    # read inputs and filter relevant columns and relevant core_metric_variables (i.e. the years to consider)
+    dictionary_list = []
+    for file_path in input_file_list:
+        file_path_val = pathlib.Path(file_path)
+        dictionary_list.append(filter_input_file(file_path_val, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep))
+
+    atb_input_df_2022, atb_input_df_2024 = dictionary_list[0], dictionary_list[1]
 
     # normalize Fixed O&M by CAPEX (or Additional OCC for retrofit technologies)
     atb_input_df_2022["value"] = atb_input_df_2022.apply(lambda x: calculate_fom_percentage(x, atb_input_df_2022), axis=1)
@@ -104,23 +90,34 @@ def pre_process_input_file(input_file_list, list_years, list_columns_to_keep, li
     concatenate_columns(atb_input_df_2022, "technology_alias_detail", ["technology_alias", "techdetail"])
     concatenate_columns(atb_input_df_2024, "technology_alias_detail", ["technology_alias", "techdetail"])
 
+    # replace technology_alias_detail with PyPSA technology names
     technology_conversion_dict = {
         "Coal_newAvgCF2ndGen": "coal",
+        "Coal_*": "coal",
         "Natural_Gas_CTAvgCF": "CCGT",
+        "Natural_Gas_*": "CCGT",
         "Hydropower_NPD1": "hydro",
         "Hydropower_NSD1": "ror",
+        #"Hydropower_*": "hydro", # TODO: this values need to be duplicated because Hydropower_* becomes both hydro and ror
         "Pumped_Storage_Hydropower_NatlClass1": "PHS",
         "Nuclear_Nuclear": "nuclear",
+        "Nuclear_*": "nuclear",
         "Geothermal_HydroFlash": "geothermal",
+        "Geothermal_*": "geothermal",
         "Land-Based_Wind_Class1": "onwind",
+        "Land-Based_Wind_*": "onwind",
         "Offshore_Wind_Class1": "offwind",
+        "Offshore_Wind_*": "offwind",
         "Utility_PV_Class1": "solar-utility",
+        "Utility_PV_*": "solar-utility",
         "Commercial_PV_Class1": "solar-rooftop",
+        "Commercial_PV_*": "solar-rooftop",
         "Utility-Scale_Battery_Storage_4Hr_Battery_Storage": "battery storage",
         "Biopower_Dedicated": "biomass",
+        "Biopower_*": "biomass",
+        "CSP_Class2": "csp-tower",
+        "CSP_*": "csp-tower",
     }
-
-    # replace technology_alias_detail with PyPSA technology names
     replace_value_name(atb_input_df_2022, technology_conversion_dict, "technology_alias_detail")
     replace_value_name(atb_input_df_2024, technology_conversion_dict, "technology_alias_detail")
 
@@ -128,6 +125,7 @@ def pre_process_input_file(input_file_list, list_years, list_columns_to_keep, li
     atb_input_df_2022["source"] = nrel_source
     atb_input_df_2024["source"] = nrel_source
 
+    # rename columns and consider just columns to keep for output (**)
     column_rename_dict = {
         "technology_alias_detail": "technology",
         "core_metric_parameter": "parameter",
@@ -137,11 +135,11 @@ def pre_process_input_file(input_file_list, list_years, list_columns_to_keep, li
         "core_metric_case": "financial_case"
     }
 
-    tuple_output_columns_to_keep = ("technology_alias_detail", "core_metric_parameter", "value", "units", "source", "display_name", "atb_year", "scenario", "core_metric_case", "core_metric_variable")
-
+    tuple_output_columns_to_keep = ("technology_alias_detail", "core_metric_parameter", "value", "units", "source", "display_name", "atb_year", "scenario", "core_metric_case", "core_metric_variable", "tax_credit_case")
     atb_input_df_2022 = atb_input_df_2022.loc[:, tuple_output_columns_to_keep].rename(columns=column_rename_dict)
     atb_input_df_2024 = atb_input_df_2024.loc[:, tuple_output_columns_to_keep].rename(columns=column_rename_dict)
 
+    # replace parameter with PyPSA cost parameter names
     parameter_conversion_dict = {
         "CAPEX": "investment",
         "Fixed O&M": "FOM",
@@ -150,12 +148,10 @@ def pre_process_input_file(input_file_list, list_years, list_columns_to_keep, li
         "Additional OCC": "investment",
         "WACC Real": "discount rate"
     }
-
-    # replace parameter with PyPSA cost parameter names
     replace_value_name(atb_input_df_2022, parameter_conversion_dict, "parameter")
     replace_value_name(atb_input_df_2024, parameter_conversion_dict, "parameter")
 
-    # Filter the technolies in atb_input_* with those in PyPSA
+    # Filter the technologies in atb_input_* with those in PyPSA
     # These rows should be replacing existing values in the next costs_....csv files
     # The rows which do not correspond to pypsa technologies are to be appended to the costs_....csv files
 
--- test/test_compile_cost_assumptions_nrel.py
+++ test/test_compile_cost_assumptions_nrel.py
@@ -4,20 +4,39 @@
 import pathlib
 import pytest
 import sys
+import pandas as pd
 
 sys.path.append("./scripts")
 
-
 from test.conftest import get_config_dict
-from compile_cost_assumptions_nrel import read_atb_input_file
+from compile_cost_assumptions_nrel import filter_input_file, replace_value_name
 
 path_cwd = pathlib.Path.cwd()
 
 @pytest.mark.parametrize(
-    "year,expected",
-    [(2022, (286998, 14)), (2024, (572232, 19))],
+    "year, expected",
+    [(2022, (3098, 12)), (2024, (20334, 12))],
 )
-def test_read_atb_input_file(year, expected):
-    list_inputs = [pathlib.Path(path_cwd, "inputs", "atb_e_{}.parquet".format(year))]
-    input_file = read_atb_input_file(list_inputs)
-    assert input_file[0].shape == expected
+def test_filter_input_file(get_config_dict, year, expected):
+    """
+    Verify what returned by filter_input_file.
+    """
+    config_dict = get_config_dict
+    list_years = config_dict["years"]
+    list_columns_to_keep = config_dict["nrel_atb"]["nrel_atb_columns_to_keep"]
+    list_core_metric_parameter_to_keep = config_dict["nrel_atb"]["nrel_atb_core_metric_parameter_to_keep"]
+    input_file_path = pathlib.Path(path_cwd, "inputs", "atb_e_{}.parquet".format(year))
+    input_file = filter_input_file(input_file_path, list_years, list_columns_to_keep, list_core_metric_parameter_to_keep)
+    assert input_file.shape == expected
+
+
+def test_replace_value_name():
+    """
+    Verify what returned by replace_value_name.
+    """
+    test_df = pd.DataFrame({"Name": ["Tom", "Paul", "John", "Sarah"], "Age": [31, 42, 12, 56], "Country": ["US", "DE", "UK", "IT"]})
+    reference_df = pd.DataFrame({"Name": ["Tom", "Paul", "John", "Sarah"], "Age": [31, 42, 12, 56], "Country": ["United States", "Germany", "United Kingdom", "IT"]})
+    conversion_dict = {"US": "United States", "DE": "Germany", "UK": "United Kingdom"}
+    output_df = replace_value_name(test_df, conversion_dict, "Country")
+    comparison_df = output_df.compare(reference_df)
+    assert comparison_df.empty

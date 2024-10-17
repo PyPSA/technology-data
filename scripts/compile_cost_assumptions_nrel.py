af11cb0476ceb595535b914bb1e29563752a8ee1
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-10-17 17:36:35 +0200

    code: remove WACC_Real

--- scripts/compile_cost_assumptions_nrel.py
+++ scripts/compile_cost_assumptions_nrel.py
@@ -7,43 +7,85 @@ import numpy as np
 from _helpers import mock_snakemake
 
 
+def get_convertion_dictionary(flag):
+    if flag.casefold() == "parameter":
+        return {
+            "CAPEX": "investment",
+            "Fixed O&M": "FOM",
+            "Variable O&M": "VOM",
+            "Fuel": "fuel",
+            "Additional OCC": "investment",
+            "WACC Real": "discount rate"
+        }
+    elif flag.casefold() == "technology":
+        return {
+            "Coal_Coal-new": "coal",
+            "Coal_newAvgCF2ndGen": "coal",
+            "Natural_Gas_NG_Combustion_Turbine_(F-Frame)": "CCGT",
+            "Natural_Gas_CTAvgCF": "CCGT",
+            "Hydropower_NPD1": "hydro",
+            "Hydropower_NSD1": "ror",
+            "Pumped_Storage_Hydropower_NatlClass1": "PHS",
+            "Nuclear_Large": "nuclear",
+            "Nuclear_Nuclear": "nuclear",
+            "Geothermal_HydroFlash": "geothermal",
+            "Land-Based_Wind_Class1": "onwind",
+            "Offshore_Wind_Class1": "offwind",
+            "Utility_PV_Class1": "solar-utility",
+            "Commercial_PV_Class1": "solar-rooftop",
+            "Utility-Scale_Battery_Storage_4Hr_Battery_Storage": "battery storage",
+            "Biopower_Dedicated": "biomass",
+            "CSP_Class2": "csp-tower",
+        }
+    elif flag.casefold() == "output_column":
+        return {
+            "display_name": "technology",
+            "core_metric_parameter": "parameter",
+            "units": "unit",
+            "atb_year": "currency_year",
+            "core_metric_case": "financial_case"
+        }
+    else:
+        raise Exception("{} is not among the allowed choices: parameter, technology, output_column")
+
+
 def filter_input_file(input_file_path, year, list_columns_to_keep, list_core_metric_parameter_to_keep):
 
-    atb_input_df = pd.read_parquet(input_file_path)
+    atb_file_df = pd.read_parquet(input_file_path)
     list_core_metric_parameter_to_keep = [str(x).casefold() for x in list_core_metric_parameter_to_keep]
     year_string = str(year).casefold()
 
     # --> select columns
     for column_name in list_columns_to_keep:
-        if column_name not in atb_input_df:
+        if column_name not in atb_file_df:
             print("missing column", column_name)
-            atb_input_df[column_name] = "not_available"
+            atb_file_df[column_name] = pd.Series(dtype="str")
         else:
             pass
-    atb_input_df = atb_input_df.loc[:, list_columns_to_keep]
+    atb_file_df = atb_file_df.loc[:, list_columns_to_keep]
 
     # --> select rows based on core_metric_parameter
     # Note: we do not apply any selection on core_metric_case and scenario.
     # Such selection can be done in the model config (e.g. PyPSA-Earth)
-    atb_input_df = atb_input_df.loc[atb_input_df["core_metric_parameter"].str.casefold().isin(list_core_metric_parameter_to_keep)]
+    atb_file_df = atb_file_df.loc[atb_file_df["core_metric_parameter"].str.casefold().isin(list_core_metric_parameter_to_keep)]
 
     # --> select rows based on core_metric_variable
     if input_file_path.name == "atb_e_2022.parquet":
         # Note: 2020 data are fetched from the input file atb_e_2022.
-        atb_input_df = atb_input_df.loc[atb_input_df["core_metric_variable"].astype(str).str.casefold() == year_string]
+        atb_file_df = atb_file_df.loc[atb_file_df["core_metric_variable"].astype(str).str.casefold() == year_string]
     elif input_file_path.name == "atb_e_2024.parquet":
         # Note: 2025, 2030, 2035, 2040, 2045, 2050 data are fetched from the input file atb_e_2024.*
-        atb_input_df = atb_input_df.loc[atb_input_df["core_metric_variable"].astype(str).str.casefold() == year_string]
+        atb_file_df = atb_file_df.loc[atb_file_df["core_metric_variable"].astype(str).str.casefold() == year_string]
     else:
         raise Exception("{} - the input file considered is not among the needed ones: atb_e_2022.parquet, atb_e_2024.parquet".format(input_file_path.stem))
 
     # --> drop duplicated rows
-    atb_input_df = atb_input_df.drop_duplicates(keep="first")
+    atb_file_df = atb_file_df.drop_duplicates(keep="first")
 
     # --> remove technology AEO
-    atb_input_df = atb_input_df.query("technology.str.casefold() != 'aeo'")
+    atb_file_df = atb_file_df.query("technology.str.casefold() != 'aeo'")
 
-    return atb_input_df
+    return atb_file_df
 
 
 def calculate_fom_percentage(x, dataframe):
@@ -67,25 +109,9 @@ def replace_value_name(dataframe, conversion_dict, column_name):
     return dataframe
 
 
-def concatenate_columns(dataframe, column_name, column_name_list):
-    dataframe[column_name] = dataframe[column_name_list].astype(str).agg('_'.join, axis=1)
-    dataframe[column_name] = dataframe[column_name].str.replace(" ", "_")
-    return dataframe
-
-
-def repeat_values(dataframe, values_list, column_name, value_to_filter, n_repeat):
-    dataframe_to_repeat = dataframe.loc[dataframe[column_name].astype(str).str.casefold() == str(value_to_filter).casefold()]
-    dataframe_to_keep = dataframe.loc[dataframe[column_name].astype(str).str.casefold() != str(value_to_filter).casefold()]
-    dataframe_repeated = pd.DataFrame(np.repeat(dataframe_to_repeat.values, n_repeat, axis=0), columns=dataframe_to_repeat.columns)
-    for i_rep in range(n_repeat):
-        dataframe_repeated.loc[i_rep::n_repeat, column_name] = values_list[i_rep]
-    return pd.concat([dataframe_to_keep, dataframe_repeated], ignore_index=True)
-
-
 def pre_process_input_file(input_file_path, year, list_columns_to_keep, list_core_metric_parameter_to_keep, nrel_source):
 
-    # read inputs and filter relevant columns and relevant core_metric_variables (i.e. the years to consider)
-
+    # Read inputs and filter relevant columns and relevant core_metric_variables (i.e. the years to consider)
     atb_input_df = filter_input_file(pathlib.Path(input_file_path), year, list_columns_to_keep, list_core_metric_parameter_to_keep)
 
     # Normalize Fixed O&M by CAPEX (or Additional OCC for retrofit technologies)
@@ -94,77 +120,28 @@ def pre_process_input_file(input_file_path, year, list_columns_to_keep, list_cor
     # Modify the unit of the normalized Fixed O&M to %-yr
     atb_input_df["units"] = atb_input_df.apply(lambda x: "%-yr" if x["core_metric_parameter"].casefold() == "fixed o&m" else x["units"], axis=1)
 
-    # Create a new column technology_alias_detail from the concatenation of the technology_alias and techdetail columns
-    concatenate_columns(atb_input_df, "technology_alias_detail", ["technology_alias", "techdetail"])
-
-    # For WACC_Real, Hydropower provides just one row. This one row has to be associated with all types of hydropower
-    # The one WACC_Real value is replicated such that each type of hydropower has its own WACC_Real entry
-    atb_input_df = repeat_values(atb_input_df, ["Hydropower_NPD1", "Hydropower_NSD1"], "technology_alias_detail", "Hydropower_*", 2)
-
     # Replace technology_alias_detail with PyPSA technology names
-    technology_conversion_dict_atb = {
-        "Coal_Coal-new": "coal",
-        "Coal_newAvgCF2ndGen": "coal",
-        "Coal_*": "coal",
-        "Natural_Gas_NG_Combustion_Turbine_(F-Frame)": "CCGT",
-        "Natural_Gas_CTAvgCF": "CCGT",
-        "Natural_Gas_*": "CCGT",
-        "Hydropower_NPD1": "hydro",
-        "Hydropower_NSD1": "ror",
-        "Pumped_Storage_Hydropower_NatlClass1": "PHS",
-        "Pumped_Storage_Hydropower_*": "PHS",
-        "Nuclear_Large": "nuclear",
-        "Nuclear_Nuclear": "nuclear",
-        "Nuclear_*": "nuclear",
-        "Geothermal_HydroFlash": "geothermal",
-        "Geothermal_*": "geothermal",
-        "Land-Based_Wind_Class1": "onwind",
-        "Land-Based_Wind_*": "onwind",
-        "Offshore_Wind_Class1": "offwind",
-        "Offshore_Wind_*": "offwind",
-        "Utility_PV_Class1": "solar-utility",
-        "Utility_PV_*": "solar-utility",
-        "Commercial_PV_Class1": "solar-rooftop",
-        "Commercial_PV_*": "solar-rooftop",
-        "Utility-Scale_Battery_Storage_4Hr_Battery_Storage": "battery storage",
-        "Biopower_Dedicated": "biomass",
-        "Biopower_*": "biomass",
-        "CSP_Class2": "csp-tower",
-        "CSP_*": "csp-tower",
-    }
-    replace_value_name(atb_input_df, technology_conversion_dict_atb, "technology_alias_detail")
+    technology_conversion_dict_atb = get_convertion_dictionary("technology")
+    atb_input_df = replace_value_name(atb_input_df, technology_conversion_dict_atb, "display_name")
 
     # Add source column
     atb_input_df["source"] = nrel_source
 
-    # Rename columns and consider just columns used in PyPSA
-    column_rename_dict = {
-        "technology_alias_detail": "technology",
-        "core_metric_parameter": "parameter",
-        "units": "unit",
-        "display_name": "further description",
-        "atb_year": "currency_year",
-        "core_metric_case": "financial_case"
-    }
+    # Add further description column
+    atb_input_df["further description"] = pd.Series(dtype="str")
 
-    tuple_output_columns_to_keep = ("technology_alias_detail", "core_metric_parameter", "value", "units", "source", "display_name", "atb_year", "scenario", "core_metric_case", "core_metric_variable", "tax_credit_case")
+    # Rename columns and consider just columns used in PyPSA
+    column_rename_dict = get_convertion_dictionary("output_column")
+    tuple_output_columns_to_keep = ("display_name", "core_metric_parameter", "value", "units", "source", "further description", "atb_year", "scenario", "core_metric_case", "core_metric_variable", "tax_credit_case")
     atb_input_df = atb_input_df.loc[:, tuple_output_columns_to_keep].rename(columns=column_rename_dict)
 
     # Replace parameter with PyPSA cost parameter names
-    parameter_conversion_dict = {
-        "CAPEX": "investment",
-        "Fixed O&M": "FOM",
-        "Variable O&M": "VOM",
-        "Fuel": "fuel",
-        "Additional OCC": "investment",
-        "WACC Real": "discount rate"
-    }
-    replace_value_name(atb_input_df, parameter_conversion_dict, "parameter")
+    parameter_conversion_dict = get_convertion_dictionary("parameter")
+    atb_input_df = replace_value_name(atb_input_df, parameter_conversion_dict, "parameter")
 
     return atb_input_df
 
 def update_cost_values(cost_dataframe, atb_dataframe):
-
     pass
 
 
@@ -185,10 +162,10 @@ if __name__ == "__main__":
     for year_val in year_list:
 
         # get the cost file to modify
-        cost_path_list = [path for path in snakemake.input.cost_files_to_modify if str(year_val) in path]
-        if len(cost_path_list) == 1:
-            cost_path = cost_path_list[0]
-            cost_df = pd.read_csv(cost_path).reset_index()
+        input_cost_path_list = [path for path in snakemake.input.cost_files_to_modify if str(year_val) in path]
+        if len(input_cost_path_list) == 1:
+            input_cost_path = input_cost_path_list[0]
+            cost_df = pd.read_csv(input_cost_path).reset_index()
         else:
             raise Exception("Please verify the list of cost files. It may contain duplicates.")
 
@@ -196,16 +173,28 @@ if __name__ == "__main__":
         if year_val == 2020:
             # choose atb_e_2022
             input_file_atb = input_file_list_atb[0]
-            atb_input_df = pre_process_input_file(input_file_atb, year_val, nrel_atb_columns_to_keep,
+            atb_e_df = pre_process_input_file(input_file_atb, year_val, nrel_atb_columns_to_keep,
                                    nrel_atb_core_metric_parameter_to_keep, nrel_atb_source_link)
+            atb_e_df.to_csv("{}_data.csv".format(year_val))
         elif year_val in year_list[1:]:
             # choose atb_e_2024
             input_file_atb = input_file_list_atb[1]
-            atb_input_df = pre_process_input_file(input_file_atb, year_val, nrel_atb_columns_to_keep,
+            atb_e_df = pre_process_input_file(input_file_atb, year_val, nrel_atb_columns_to_keep,
                                    nrel_atb_core_metric_parameter_to_keep, nrel_atb_source_link)
+            atb_e_df.to_csv("{}_data.csv".format(year_val))
         else:
             raise Exception("{} is not a considered year".format(year_val))
 
         # update the cost file
-        update_cost_values(cost_df, atb_input_df)
+        update_cost_values(cost_df, atb_e_df)
+
+        # output the modified cost file
+        output_cost_path_list = [path for path in snakemake.output if str(year_val) in path]
+        if len(output_cost_path_list) == 1:
+            output_cost_path = output_cost_path_list[0]
+            cost_df.to_csv(output_cost_path)
+        else:
+            raise Exception("Please verify the list of cost files. It may contain duplicates.")
+
+
 
--- test/test_compile_cost_assumptions_nrel.py
+++ test/test_compile_cost_assumptions_nrel.py
@@ -9,13 +9,13 @@ import pandas as pd
 sys.path.append("./scripts")
 
 from test.conftest import get_config_dict
-from compile_cost_assumptions_nrel import calculate_fom_percentage, concatenate_columns, filter_input_file, pre_process_input_file, repeat_values, replace_value_name
+from compile_cost_assumptions_nrel import calculate_fom_percentage, filter_input_file, pre_process_input_file, replace_value_name
 
 path_cwd = pathlib.Path.cwd()
 
 @pytest.mark.parametrize(
     "file_year, year, expected",
-    [(2019, 2020, "atb_e_2019 - the input file considered is not among the needed ones: atb_e_2022.parquet, atb_e_2024.parquet"), (2022, 2020, (3092, 12)), (2024, 2025, (3213, 12)), (2024, 2030, (3405, 12)), (2024, 2035, (3429, 12)), (2024, 2040, (3429, 12)), (2024, 2045, (3429, 12)), (2024, 2050, (3429, 12))],
+    [(2019, 2020, "atb_e_2019 - the input file considered is not among the needed ones: atb_e_2022.parquet, atb_e_2024.parquet"), (2022, 2020, (3002, 11)), (2024, 2025, (3126, 11)), (2024, 2030, (3312, 11)), (2024, 2035, (3336, 11)), (2024, 2040, (3336, 11)), (2024, 2045, (3336, 11)), (2024, 2050, (3336, 11))],
 )
 def test_filter_input_file(get_config_dict, file_year, year, expected):
     """
@@ -28,6 +28,8 @@ def test_filter_input_file(get_config_dict, file_year, year, expected):
     if file_year in [2022, 2024]:
         input_file = filter_input_file(input_file_path, year, list_columns_to_keep, list_core_metric_parameter_to_keep)
         assert input_file.shape == expected
+        assert "aeo" not in input_file["technology"].astype(str).str.casefold().unique()
+
     else:
         with pytest.raises(Exception) as excinfo:
             input_file = filter_input_file(input_file_path, year, list_columns_to_keep,
@@ -46,46 +48,6 @@ def test_replace_value_name():
     comparison_df = output_df.compare(reference_df)
     assert comparison_df.empty
 
-
-def test_concatenate_columns():
-    """
-    Verify what returned by concatenate_columns.
-    """
-    test_df = pd.DataFrame({"Name": ["Tom", "Paul", "Sarah"], "Age": [31, 42, 56]})
-    reference_df = pd.DataFrame({"Name": ["Tom", "Paul", "Sarah"], "Age": [31, 42, 56], "Name_Age": ["Tom_31", "Paul_42", "Sarah_56"]})
-    output_df = concatenate_columns(test_df, "Name_Age", ["Name", "Age"])
-    comparison_df = output_df.compare(reference_df)
-    assert comparison_df.empty
-
-
-def test_repeat_values():
-    """
-    Verify what returned by repeat_values.
-    """
-    test_df = pd.DataFrame({"Name": ["Tom", "Paul", "Sarah", "Sabrina"], "Age": [31, 31, 31, 12],
-                            "City": ["Rome", "Rome", "Rome", "Berlin"]})
-
-    # Numeric values
-    numeric_reference_df = pd.DataFrame(
-        {"Name": ["Sabrina", "Tom", "Tom", "Tom", "Paul", "Paul", "Paul", "Sarah", "Sarah", "Sarah"],
-         "Age": [12, 40, 41, 42, 40, 41, 42, 40, 41, 42],
-         "City": ["Berlin", "Rome", "Rome", "Rome", "Rome", "Rome", "Rome", "Rome", "Rome", "Rome"]
-         })
-    numeric_output_df = repeat_values(test_df, [40, 41, 42], "Age", 31, 3)
-    numeric_comparison_df = numeric_output_df.compare(numeric_reference_df)
-
-    # String values
-    string_reference_df = pd.DataFrame(
-        {"Name": ["Sabrina", "Tom", "Tom", "Tom", "Paul", "Paul", "Paul", "Sarah", "Sarah", "Sarah"],
-         "Age": [12, 31, 31, 31, 31, 31, 31, 31, 31, 31],
-         "City": ["Berlin", "Munich", "Paris", "Mainz", "Munich", "Paris", "Mainz", "Munich", "Paris", "Mainz"]
-         })
-    string_output_df = repeat_values(test_df, ["Munich", "Paris", "Mainz"], "City", "rome", 3)
-    string_comparison_df = string_output_df.compare(string_reference_df)
-
-    assert numeric_comparison_df.empty
-    assert string_comparison_df.empty
-
 @pytest.mark.parametrize(
     "display_name, expected",
     [("Coal-new", 2.13), ("Coal-95%-CCS", 2.06), ("Coal-99%-CCS", 2.05), ("Coal-IGCC", 2.38), ("Coal-IGCC-90%-CCS", 2.37), ("Coal integrated retrofit 90%-CCS", 7.37), ("Coal integrated retrofit 95%-CCS", 7.22)],
@@ -99,7 +61,7 @@ def test_calculate_fom_percentage(display_name, expected):
     assert test_df.loc[(test_df["display_name"] == display_name) & (test_df["core_metric_parameter"] == "Fixed O&M")]["value"].item() == expected
 
 @pytest.mark.parametrize(
-    "input_file_year, year, expected", [(2022, 2020, (3098, 11)), (2024, 2050, (3435, 11))],
+    "input_file_year, year, expected", [(2022, 2020, (3002, 11)), (2024, 2050, (3336, 11))],
 )
 def test_pre_process_input_file(get_config_dict, input_file_year, year, expected):
     config_dict = get_config_dict
@@ -108,7 +70,7 @@ def test_pre_process_input_file(get_config_dict, input_file_year, year, expected
     nrel_atb_core_metric_parameter_to_keep = config_dict["nrel_atb"]["nrel_atb_core_metric_parameter_to_keep"]
     nrel_atb_source_link = config_dict["nrel_atb"]["nrel_atb_source_link"]
     output_df = pre_process_input_file(input_file_path, year, nrel_atb_columns_to_keep, nrel_atb_core_metric_parameter_to_keep, nrel_atb_source_link)
-    reference_parameter_list = sorted(["investment", "CF", "FOM", "VOM", "fuel", "discount rate"])
+    reference_parameter_list = sorted(["investment", "CF", "FOM", "VOM", "fuel"])
     output_parameter_list = sorted(list(output_df["parameter"].unique()))
     assert output_df.shape == expected
     assert len(output_parameter_list) == len(reference_parameter_list)

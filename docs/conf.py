e9a40d69476351e6a07983c53e2bf83ca37fda9a
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2025-01-10 15:57:07 +0100

    change conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 
 import os
 import sys
+import sphinx
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -335,4 +336,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+if sphinx.version_info[0] < 8:
+    intersphinx_mapping = {"http://docs.python.org/": None}
+else:
+    intersphinx_mapping = {'python': ('https://docs.python.org/', None)}

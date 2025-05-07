e6359ef86b3b2451dee984d6bfb4c55ecc77e5a7
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2025-05-07 14:00:02 +0200

    code: modify test_source_loading to include newly modified pytest.fixture

--- test/conftest.py
+++ test/conftest.py
@@ -9,10 +9,20 @@ sys.path.append("./technology-data")
 path_cwd = pathlib.Path.cwd()
 
 
-@pytest.fixture()
-def example_source():
+@pytest.fixture(scope="function")
+def example_source(request):
     """Fixture to create an example source."""
-    return td.Source(
-        "example01",
-        pathlib.Path(path_cwd, "technologydata", "datasources", "example01"),
+    source_name = request.param.get("source_name", "example01")
+    source_path = request.param.get(
+        "source_path", pathlib.Path("technologydata", "datasources", "example01")
     )
+
+    def load_example_source():
+        """Inner function to create the source."""
+        return td.Source(
+            source_name,
+            pathlib.Path(path_cwd, source_path),
+        )
+
+    # Call the inner function and return the result
+    return load_example_source()
--- test/test_source_loading.py
+++ test/test_source_loading.py
@@ -2,12 +2,12 @@
 
 import pathlib
 import sys
-from pathlib import Path
 
 import pandas as pd
 import pytest
 
 import technologydata as td
+from technologydata import Source
 
 sys.path.append("./technology-data")
 path_cwd = pathlib.Path.cwd()
@@ -16,7 +16,7 @@ path_cwd = pathlib.Path.cwd()
 def test_source_loading():
     """Simple approach for custom user sources."""
     name = "example01"
-    fp = Path("technologydata/datasources/example01")
+    fp = pathlib.Path(path_cwd, "technologydata", "datasources", "example01")
 
     # Check if the source can be loaded from a string or a path
     assert td.Source(name, fp)
@@ -25,40 +25,78 @@ def test_source_loading():
     assert td.Source(name, str(fp.absolute()))
 
 
-def test_source_initialization(example_source):
+@pytest.mark.parametrize(
+    "example_source, expected_path, expected_features",
+    [
+        (
+            {
+                "source_name": "example01",
+                "source_path": pathlib.Path(
+                    "technologydata", "datasources", "example01"
+                ),
+            },
+            pathlib.Path(path_cwd, "technologydata", "datasources", "example01"),
+            ["Technologies"],
+        ),
+        (
+            {
+                "source_name": "example02",
+                "source_path": pathlib.Path(
+                    "technologydata", "datasources", "example02"
+                ),
+            },
+            pathlib.Path(path_cwd, "technologydata", "datasources", "example02"),
+            ["Technologies"],
+        ),
+    ],
+    indirect=["example_source"],
+)
+def test_source_initialization(example_source, expected_path, expected_features):
     """Test the initialization of the Source class."""
-    assert example_source.name == "example01"
     # Path should be the provided path
-    assert example_source.path == Path(
-        path_cwd, "technologydata", "datasources", "example01"
-    )
+    assert example_source.path == expected_path
     # Check datatype
     assert isinstance(example_source.details, pd.DataFrame)
     # Ensure the DataFrame is not empty
     assert not example_source.details.empty
     # Check for expected features
-    assert example_source.available_features == ["Technologies"]
+    assert example_source.available_features == expected_features
     # Check whether packaged sources can also be loaded
     assert (
-        td.Source("example01").name == example_source.name
-        and td.Source("example01").path.absolute() == example_source.path.absolute()
+        td.Source(example_source.name).name == example_source.name
+        and td.Source(example_source.name).path.absolute()
+        == example_source.path.absolute()
     )
 
 
+@pytest.mark.parametrize(
+    "example_source",
+    [
+        {
+            "source_name": "example01",
+            "source_path": pathlib.Path("technologydata", "datasources", "example01"),
+        },
+        {
+            "source_name": "example02",
+            "source_path": pathlib.Path("technologydata", "datasources", "example02"),
+        },
+    ],
+    indirect=True,
+)
 def test_sources_initialization(example_source):
     """Test different ways of initializing a Sources object."""
     # See if we can directly load all packaged sources directly / load from dict
     assert td.Sources(td.AVAILABLE_SOURCES)
     # Load a single prepackaged source by name
-    assert td.Sources(["example01"])
+    assert td.Sources([example_source.name])
     # Load single source not as list
-    assert td.Sources("example01")
+    assert td.Sources(example_source.name)
     # Convert a single source into a Sources object
-    assert td.Sources(td.Source("example01"))
+    assert td.Sources(td.Source(example_source.name))
     # Combine multiple loaded sources into one
     assert td.Sources([example_source, example_source])
     # Mixed loading of named and loaded sources
-    assert td.Sources(["example01", example_source])
+    assert td.Sources([example_source.name, example_source])
 
 
 @pytest.mark.parametrize(

91d9057061b5d251829b4753adcac74abf361747
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2025-05-08 13:50:26 +0200

    code: update test/test_adjust_functions/forecast01/sources.csv

--- test/conftest.py
+++ test/conftest.py
@@ -22,14 +22,14 @@ path_cwd = pathlib.Path.cwd()
 
 
 @pytest.fixture(scope="function")
-def example_source(request):
+def example_source(request) -> td.Source:
     """Fixture to create an example source."""
     source_name = request.param.get("source_name", "example01")
     source_path = request.param.get(
         "source_path", pathlib.Path("technologydata", "datasources", "example01")
     )
 
-    def load_example_source():
+    def load_example_source() -> td.Source:
         """Inner function to create the source."""
         return td.Source(
             source_name,

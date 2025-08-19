0f0b43af9d123c99e8248b884d73db81539068dd
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2025-08-19 11:06:36 +0200

    code: update unit test in test_source.py

--- test/test_source.py
+++ test/test_source.py
@@ -153,7 +153,7 @@ class TestSource:
 
     def test_store_in_wayback(self) -> None:
         """Check if a given url is correctly stored as a snapshot on Internet Archive Wayback Machine."""
-        url_to_archive = "https://www.openenergytransition.org/outputs.html"
+        url_to_archive = "https://openenergytransition.org/outputs.html"
         archived_info = technologydata.Source.store_in_wayback(url_to_archive)
 
         # Check if archived_info is None
